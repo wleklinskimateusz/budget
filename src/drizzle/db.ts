0bbf2d0ab7ebaa7bed7f1a5bf7c81d34808fc859
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2024-06-14 14:39:42 +0200

    chore: bump next and fix build error

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,6 +1,9 @@
-import "@/drizzle/envConfig";
+"use server";
+
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
+
+import "@/drizzle/envConfig";
 
 export const db = drizzle(sql, { schema });
