8def9a9cd260805e87e122098c823c487fb10a74
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2024-07-12 14:24:21 +0200

    feat: configure sotrybook and add logo story

--- src/app/(marketing)/_components/footer.tsx
+++ src/app/(marketing)/_components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full border-t bg-slate-100 p-4">
       <div className="mx-auto flex w-full items-center justify-between md:max-w-screen-2xl">
-        <Logo />
+        <Logo className="hidden md:flex" />
         <div className="flex w-full items-center justify-between space-x-4 md:block md:w-auto">
           <Button size="sm" variant="ghost">
             Privacy Policy
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,10 +3,15 @@ import Image from "next/image";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 
-export const Logo = () => {
+export const Logo = ({ className }: { className?: string }) => {
   return (
     <Link href="/">
-      <div className="hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
+      <div
+        className={cn(
+          " items-center gap-x-2 transition hover:opacity-75",
+          className,
+        )}
+      >
         <Image src="/logo.svg" alt="Logo" width={30} height={30} />
         <p className={cn("pb-1 text-lg text-neutral-700")}>KoderKash</p>
       </div>
